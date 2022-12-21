0e57390651b37e6b5718825a19e1377987e2dd0a
ibraidab <ibraidab@users.noreply.github.com>
2022-12-21 10:29:13 -0300

    cambios_7b

--- pages/api/sqlPlanetScale.ts
+++ pages/api/sqlPlanetScale.ts
@@ -2,7 +2,7 @@
 
 import { NextApiRequest, NextApiResponse } from "next";
 
-import sql from "mssql";
+const sql = require("mssql2");
 
 export default async function handler(
   req: NextApiRequest,
