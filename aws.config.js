54f278a4a12bedd4158687812654423c770d1c30
braidata <ibraida@braidata.com>
2024-05-09 07:04:58 +0000

    dev 1b

---
+++ aws.config.js
@@ -0,0 +1,10 @@
+// aws.config.js
+import AWS from 'aws-sdk';
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+export default AWS;
